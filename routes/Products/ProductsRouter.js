96a8d17b32974c99c745ff032b399272ef5ee510
marra abdallah <abdallah4marra@gmail.com>
2024-05-02 21:40:01 +0100

    changes

--- routes/Products/ProductsRouter.js
+++ routes/Products/ProductsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productsController = require('../../Controllers/productsController')
+const productsController = require('../../controllers/productsController')
 const AMauthorization = require('../../middlewares/AuthAM')
 const upload = require('../../middlewares/Cloudinary')
 
@@ -8,13 +8,10 @@ router.post('/products',upload.single('product_image'), productsController.creat
 
 router.get('/allproducts', productsController.findProducts)
 
-// router.get('/produc', productsController.searchProducts)
-
 router.get('/products/:id', productsController.getProductById)
 
 router.patch('/products/:id',upload.single('productImage'), productsController.updateProduct)
 
 router.delete('/products/:id', productsController.removeProduct)
-// router.get('/products/sorts', productsController.sortProducts)
 
 module.exports = router
